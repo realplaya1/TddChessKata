15b6bb5f50a6fb612b6987dd074c15467e0a8cef
realplaya1 <39435205+realplaya1@users.noreply.github.com>
2020-11-22 11:57:17 +0300

    commit

--- TddChessEngineLib/Pawn.cs
+++ TddChessEngineLib/Pawn.cs
@@ -4,6 +4,23 @@ namespace TddChessEngineLib
 {
     public class Pawn
     {
-        public void Turn(object startPosition, object finishPosition){}
+        public string CurrentPosition {get; private set;}
+        public FigureColor  figureColor {get;}
+        public Pawn(string initialPosition, FigureColor figurecolor)
+        {
+            CurrentPosition = initialPosition;
+            figureColor = figurecolor;
+        }
+        public void Turn(string startPosition, string finishPosition)
+        {
+            CurrentPosition = finishPosition;
+        }
+
+        
+    }
+
+    public enum FigureColor
+    {
+        White, Black
     }
 }
